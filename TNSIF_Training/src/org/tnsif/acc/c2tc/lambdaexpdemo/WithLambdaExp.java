ed3dc5de084aeba2afd61ef8d88c6bda67987fe7
Sushmitha A R <sushmithareddy9901@gmail.com>
2026-08-28 10:57:11 +0530

    First commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/lambdaexpdemo/WithLambdaExp.java
@@ -0,0 +1,17 @@
+package org.tnsif.acc.c2tc.lambdaexpdemo;
+interface Drawable1
+{
+	public void draw();
+}
+public class WithLambdaExp {
+
+	public static void main(String[] args) {
+		int width=10;
+		Drawable1 obj=()->{
+			System.out.println("Drawing a width"+width);
+		};
+		obj.draw();
+
+	}
+
+}
